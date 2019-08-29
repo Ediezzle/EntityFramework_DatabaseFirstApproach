e66d618fec5c324a89769039ee104095434a18d7
Ediezzle <edmore.mandikiyana.gmail.com>
2019-08-29 21:13:53 +0200

    Add project files.

---
+++ EntityFramework_DatabaseFirstApproach/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_DatabaseFirstApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
