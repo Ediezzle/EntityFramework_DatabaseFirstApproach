0e8f9d788fe4d0b5394b9a91c9ca396c94dd9deb
Ediezzle <edmore.mandikiyana.gmail.com>
2019-09-01 23:03:12 +0200

    First commit

--- EntityFramework_DatabaseFirstApproach/Program.cs
+++ EntityFramework_DatabaseFirstApproach/Program.cs
@@ -10,6 +10,17 @@ namespace EntityFramework_DatabaseFirstApproach
     {
         static void Main(string[] args)
         {
+            StudentDBEntities container = new StudentDBEntities();
+            ICollection<Students> students = new List<Students>
+            {
+                new Students() { Name = "Harry" },
+                new Students() { Name = "Jane" },
+                new Students() { Name = "Nick" }
+            };
+            container.Classes.Add(new Classes() { ClassName = "Class 1", Students = students });
+            container.SaveChanges();
+            container.Students.Add(new Students() { Class_Id = 1, Name = "Ben" });
+            container.SaveChanges();
         }
     }
 }
